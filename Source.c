8a6c8382ca9cb3f63cf81e9a2f8b187d99b4e6d1
neotouch1 <90023755+neotouch1@users.noreply.github.com>
2021-10-19 13:44:55 +0300

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+    int N, i;  // Переменные индекса и генерации счета.
+    double a, fractional_part, max, min; // Переменная генерации, дробная переменная, переменные верхней и нижней границ диапазона.
+    double num, sum = 0; // Переменная целой и переменная суммы.
+
+    printf("Input N: "); // Выводит на экран предложение ввести значение для N
+    scanf("%d", &N); 
+    printf("Input min: ");  // Выводит на экран предложение ввести значение для "min"
+    scanf("%lf", &min);
+    printf("Input max: ");  // Выводит на экран предложение ввести значение для "max"
+    scanf("%lf", &max);
+
+    for (i = 0; i < N; i++)
+    {
+        a = (double)rand() / RAND_MAX * (max - min) + min; // Происходит генерация чисел.
+        fractional_part = modf(a, &num); // Выделяем дробную часть
+
+        if (i >= 0 && i < 10 && i != (int)(fractional_part * 10)) // Если номер числа не совпадёт с его дробной частью, то оно складывается в общую сумму.
+            sum += a; 
+        else if (i >= 10 && i < 100 && i != (int)(fractional_part * 100))
+            sum += a;
+        else if (i >= 100 && i < 1000 && i != (int)(fractional_part * 1000))
+            sum += a;
+        else if (i >= 1000 && i < 10000 && i != (int)(fractional_part * 10000))
+            sum += a;
+        else if (i >= 10000 && i < 100000 && i != (int)(fractional_part * 100000))
+            sum += a;
+    }
+    printf("SUM: %f\n\n", sum); // Вывод итоговой суммы на экран.
+
+    return 0;
+}
